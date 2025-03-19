761d497f60745be065fd7a573ae045d5a8bba5ad
Samet <sas007@edu.zealand.dk>
2025-03-19 11:48:03 +0100

    Tilføjet ny funktion i index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
-//Write your Javascript code here
+function createListItem(className, idName, text){
+//Oret et nyt <li> element
+const li = document.createElement("li");
+
+//Tilføjer id-attributen
+li.setAttribute("id", idName);
+
+//Tilføjer class-attributten
+li.setAttribute("class", className);
+
+//opret en tekstnode
+const textNode = document.createTextNode(text);
+
+
+li.appendChild(textNode); //Tilføjer tekstNoden til <li>-Element 
+
+return li;
+}
 console.log("Shoppinglist")
